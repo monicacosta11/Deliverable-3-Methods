7aa2dbb7a28070f9c5d7331cf2cb63464d86165b
Monica Costa <monicacosta@usf.edu>
2019-01-28 22:45:53 -0500

    update

--- Deliverable 3 Methods/Program.cs
+++ Deliverable 3 Methods/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+Author: Monica Costa
+Date: 1/28/2019
+Comments: This C# Console application code demonstrates the use of
+  methods.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,23 @@ namespace Deliverable_3_Methods
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Please enter your first and last name: ");
+
+            try
+            {
+                //This variable will gather data from user input
+                string input = Console.ReadLine();
+
+                Console.WriteLine("Hello " + input + "!");
+                Console.ReadKey(true);
+
+            }
+            catch
+            {
+                Console.WriteLine("There was an error");
+                Console.ReadKey(true);
+            } //End of catch
         }
     }
 }
